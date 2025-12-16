6a5236df2a39b5ab39c314725ee4e16346eda578
Lawlynch <lawlynch@gmail.com>
2025-12-16 13:04:40 +0000

    Added admin dashboard and update API

---
+++ app/admin/page.js
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+  return (
+    <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1 style={{ color: 'blue' }}>Admin Panel Active</h1>
+      <p>If you can see this, the routing is working!</p>
+    </div>
+  );
+}
